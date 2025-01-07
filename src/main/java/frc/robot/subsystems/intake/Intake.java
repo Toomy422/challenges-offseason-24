b80d743a874f1f5576167512a7c989216104b388
Toomy422 <26mwood@gsgis.k12.va.us>
2025-01-06 19:06:21 -0500

    GOD DID

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,31 +1,38 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   // For instructions on how to implement this class, refer to the README.md file
+  IntakeIO m_io;
+  IntakeInputsAutoLogged inputsAutoLogged;
 
   public Intake(IntakeIO io) {
     // TODO: Implement the constructor
+    m_io = io;
+    inputsAutoLogged = new IntakeInputsAutoLogged();
   }
 
   @Override
   public void periodic() {
     // TODO: Implement this method
+    m_io.updateInputs(inputsAutoLogged);
   }
 
   public void setRollerVoltage(double voltage) {
-    // TODO: Implement this method
+    m_io.setRollerVoltage(voltage);
+    ;
   }
 
   public Command setVoltageCommand(double voltage) {
     // TODO: Implement this method
-    return null;
+    return Commands.runOnce(() -> setRollerVoltage(voltage), this);
   }
 
   public IntakeInputsAutoLogged getInputs() {
     // TODO: Implement this method
-    return null;
+    return inputsAutoLogged;
   }
 }
